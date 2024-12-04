bcf9b1944e576113246475fdfbbb45d732dce025
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-12-04 11:14:13 -0700

    Database and mvvm ui setup

---
+++ RMDesktopUserInterface/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using RMDesktopUserInterface.ViewModels;
+
+namespace RMDesktopUserInterface
+{
+    public class Bootstrapper : BootstrapperBase 
+    {
+        public Bootstrapper() 
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }   
+}
